b9c02d77fcb47ff4365bcade4e35255867134430
pfcaceres <111323556+pfcaceres@users.noreply.github.com>
2025-08-04 12:50:31 -0300

    ci-cd config

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM tomcat AS tomcat-server
+ENV TZ=America/Montevideo
+
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+COPY target/cert.war /usr/local/tomcat/webapps
--- src/main/java/com/accesa/EmploymentcertificatesApplication.java
+++ src/main/java/com/accesa/EmploymentcertificatesApplication.java
@@ -1,5 +1,8 @@
 package com.accesa;
 
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -11,6 +14,11 @@ public class EmploymentcertificatesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmploymentcertificatesApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EmploymentcertificatesApplication.class);
 	}
 
 }
